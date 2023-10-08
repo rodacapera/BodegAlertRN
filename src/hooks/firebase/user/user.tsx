5ee15d4baa7e4c36da9205a8d21f090877679d90
rodacapera <rodacapera@gmail.com>
2023-10-08 08:32:11 -0500

    feat: shop update from profile admin

--- src/hooks/firebase/user/user.tsx
+++ src/hooks/firebase/user/user.tsx
@@ -6,7 +6,20 @@ export const getUserFirebase = async (userUid: string) => {
   return dbUser.data();
 };
 
+export const editShopFirebase = async (
+  shop: {
+    alias: string;
+    address: string;
+  },
+  doc: string
+) => {
+  await firestore().collection('shops').doc(doc).update(shop);
+  return shop;
+};
+
 export const editUserFirebase = async (user: User) => {
+  const shop = {alias: user.alias, address: user.address};
   await firestore().collection('users').doc(user.user_uid).update(user);
+  await editShopFirebase(shop, user.shop.split('/')[1]);
   return user;
 };
